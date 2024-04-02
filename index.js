fce3ba0bfee6e8ac95fd4a811e176583f1163ab0
Khadim Ali <162663094+KhadimA@users.noreply.github.com>
2024-04-02 21:32:26 +0500

    Game

---
+++ index.js
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random() * 5 + 1);
+let answare;
+let usernumber;
+console.log("<------------------------------------------>");
+console.log("| Welcome To Guessing Game with Khadim Ali |");
+console.log("<------------------------------------------>");
+do {
+    answare = await inquirer.prompt([
+        {
+            name: "UGN",
+            type: "number",
+            message: "Guess a Number Between 1 and 10:",
+        },
+    ]);
+    usernumber = answare.UGN;
+    if (usernumber === randomnumber) {
+        console.log("Congratulations! You guessed the correct number.");
+    }
+    else {
+        console.log("Sorry, wrong number. Try again.");
+    }
+} while (usernumber !== randomnumber);
